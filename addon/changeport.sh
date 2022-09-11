5be152a89247b1b820461bfac4d76d7d507fabb2
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-11 15:46:33 +0700

    Update changeport.sh

--- addon/changeport.sh
+++ addon/changeport.sh
@@ -17,7 +17,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ########################
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo "Checking VPS"                                                                                                               
-IZIN=$(curl -sS https://raw.githubusercontent.com/Rocknet Store/ipvps/main/ip | awk '{print $4}' | grep $MYIP )                       
+IZIN=$(curl -sS https://raw.githubusercontent.com/sibeesans/rcv/main/ip | awk '{print $4}' | grep $MYIP )                       
 if [[ $MYIP = $IZIN ]]; then                                                                                                      
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"                                                                                
 else                                                                                                                              
